5bd1e75fe48011a3e75496e46854a37c24620ed1
Gustavo-SB <gustavosb862@gmail.com>
2021-05-05 00:28:29 -0300

    SubindoGit

---
+++ src/App.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+import { RepositoryList } from './components/RepositoryList.jsx';
+import './styles/global.scss';
+
+export function App() {
+  return <RepositoryList />
+}
